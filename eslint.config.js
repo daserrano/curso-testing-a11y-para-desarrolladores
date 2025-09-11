7198c0c00460916eb631a591077f50b62f138bd6
David Serrano <dserrano@visiotechsecurity.com>
2025-09-11 16:19:52 +0200

    modulo_3

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
 import prettierConfig from "eslint-config-prettier";
+import jsxA11Y from 'eslint-plugin-jsx-a11y';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -39,10 +40,12 @@ export default [
     },
     plugins: {
       react,
-      'react-hooks': reactHooks
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11Y,
     },
     rules: {
       ...react.configs.recommended.rules,
+      ...jsxA11Y.flatConfigs.recommended.rules,
       'react/prop-types': 'off'
     }
   },
---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+        <img src="" />
+        <button></button>
+    </div>
+  )
+}
+
+export default Test
